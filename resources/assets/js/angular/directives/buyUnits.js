0a6f4810a0ef1e64ca9b67007ae64a5d7b27ce22
leinad <xavi7th@gmail.com>
2018-08-24 00:11:02 +0100

    Remove bank payment option

--- resources/assets/js/angular/directives/buyUnits.js
+++ resources/assets/js/angular/directives/buyUnits.js
@@ -21,8 +21,7 @@ var url = `
       <div id="form" class="ui segments">
         <div class="ui segment">
           <div id="session-menu" class="ui two item menu">
-          <a class="item active" data-tab="online-transfer" style="background-color: #03A9F4; color:#FFF;">PAY ONLINE</a>
-          <a class="item" data-tab="bank-transfer">Tap here for bank deposit</a>
+            <a class="item active" data-tab="online-transfer" style="background-color: #03A9F4; color:#FFF;">PAY ONLINE</a>
           </div>
         </div>
 
@@ -59,45 +58,6 @@ var url = `
                   <img src="/img/paystack_preview.png" alt="" />
                 </p>
               </div>
-            </div>
-          </div>
-
-          <div class="ui tab" data-tab="bank-transfer">
-            <div class="actions  flex-center">
-              <div class="ui black left deny button">Close</div>
-            </div>
-            <div class="ui segment" id="extra">
-              <div class="ui small header">Pay via bank Deposit or Wire transfer(Minimum ₦500)</div>
-              <p>
-                <b style="font-weight: 900; margin-right:5%; color:red; font-size:2em;">!</b>  Use your email as depositor's name or transaction description
-              </p>
-              <p>
-                <b>Account Name:</b> Tcom Wireless Nigeria
-              </p>
-              <p>
-                <b>Account Number:</b> 1019040225 (United Bank for Africa)
-              </p>
-              <p>
-                <b>  Account type:</b> Current
-              </p>
-              <p style="width: 70px;">
-                <img style="min-width: 100%;" src="/img/uba.jpg" alt="" />
-              </p>
-
-              <div class="ui positive message">
-                <div class="header">
-                  NB:
-                </div>
-                <p>After payment, send your payment details to <a href="mailto:hello@fastplay24.com">hello@fastplay24.com</a>. Your account will be credited as soon as your payment is confirmed. If your account is not credited within 24 hours, send an email to <a href="mailto:hello@fastplay24.com">hello@fastplay24.com</a> or use the <a href="/support-center" target="_blank">support form</a>.</p>
-              </div>
-
-              <div class="ui message">
-                <div class="header">
-                  NB:
-                </div>
-                <p>When making payments, please ensure you confirm that the account number you pay into matches the one shown above as we will not be liable for any payments made to a bank account that is not ours.</p>
-              </div>
-
             </div>
           </div>
         </div>
